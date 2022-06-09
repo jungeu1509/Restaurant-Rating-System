d777d6c0d3dc81465bf083bf88ac44f01486a07f
Kenneth Jung <jungeu1509@gmail.com>
2022-06-09 15:55:26 +0900

    update. Draft commit

---
+++ src/ratingSystem/DTO/IndexCompare.java
@@ -0,0 +1,14 @@
+package ratingSystem.DTO;
+
+import java.util.Comparator;
+import ratingSystem.DTO.RestaurantDTO;
+
+public class IndexCompare implements Comparator<RestaurantDTO> {
+
+    @Override
+    public int compare(RestaurantDTO o1, RestaurantDTO o2) {
+        if(o1.getIndex() > o2.getIndex()) return 1;
+        else if(o1.getIndex() < o2.getIndex()) return -1;
+        return 0;
+    }
+}
